96fd27c95344c246b6c2a6291dde5d7e8afefc3c
Nishat <123876007+isha35-1215@users.noreply.github.com>
2023-09-16 18:25:00 +0600

    sweet alert updated

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = ({ selectedCourses,remaining,totalCount }) => {
 
     return (
         <div className='card cart'>
-            <h4>Credit Hour Remaining :{remaining}</h4>
+            <h4>Credit Hour Remaining {remaining} hr</h4>
             <h3>Course Name</h3>
             {selectedCourses.map((course,index) => (
         <ol key={course.id} className='course-list'>{`${index + 1}. ${course.courseName}`}</ol>
